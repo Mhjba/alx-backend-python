f9fa82a31309ebabd4bf71d9cc04b89c6ae5933e
Mhjba <mahjoubaaitalla15@gmail.com>
2024-07-04 22:16:12 +0200

    python variable annotations

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,9 +1,12 @@
-from typing import Callable
+#!/usr/bin/env python3
 """ multiplies a float by multiplier """
+from typing import Callable
+
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """returns a function"""
-    def f(value: float) -> float:
+
+    def f(v: float):
         """ multiplies a float by multiplier """
-        return value * multiplier
+        return v * multiplier
     return f
