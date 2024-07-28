7a971fc94bfc80d6a5c7e8836820816cf9411d34
Mhjba <mahjoubaaitalla15@gmail.com>
2024-07-28 16:40:11 +0200

    tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -35,7 +35,7 @@ class TestGithubOrgClient(unittest.TestCase):
 
     @patch("client.get_json")
     @patch("client.GithubOrgClient._public_repos_url",
-           new_callable=PropertyMock,)
+        new_callable=PropertyMock,)
     def test_public_repos(self, mock_public_repos_url, mock_get_json) -> None:
         """Tests the `public_repos` method."""
         test = GithubOrgClient("test")
@@ -55,57 +55,6 @@ class TestGithubOrgClient(unittest.TestCase):
         """Tests the `has_license` method."""
         license = GithubOrgClient.has_license(repo, license_key)
         self.assertEqual(license, output)
-
-
-class TestIntegrationGithubOrgClient(unittest.TestCase):
-    """
-    Integration test for GithubOrgClient
-    """
-
-    class setUpClass:
-        """
-        Set up class for integration test
-        """
-
-        @patch("client.get_json")
-        def setUp(self, mock_get_json):
-            """
-            Set up method for class
-            """
-            self.test_class = GithubOrgClient("test")
-            self.org_payload = {"repos_url": "www.test.com"}
-            self.repos_payload = [
-                {"name": "repo1", "license": {"key": "my_license"}},
-                {"name": "repo2", "license": {"key": "other_license"}},
-            ]
-            mock_get_json.side_effect = [
-                self.org_payload,
-                self.repos_payload,
-            ]
-
-
-@parameterized_class(
-
-    ("org_payload", "repos_payload", "expected_repos", "apache2_repos"),
-    TEST_PAYLOAD
-)
-class TestIntegrationGithubOrgClient(unittest.TestCase):
-    """Performs integration tests for the `GithubOrgClient` class."""
-
-    @classmethod
-    def setUpClass(cls) -> None:
-        """Sets up class fixtures before running tests."""
-        r_payload = {
-            "return_value.json.side_effect": [
-                cls.org_payload,
-                cls.repos_payload,
-                cls.org_payload,
-                cls.repos_payload,
-            ]
-        }
-        cls.get_patcher = patch("requests.get", **r_payload)
-
-        cls.mock = cls.get_patcher.start()
 
     def test_public_repos(self) -> None:
         """Tests the `public_repos` method."""
